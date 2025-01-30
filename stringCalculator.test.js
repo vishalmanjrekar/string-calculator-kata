40487d975ea23bf2aa34a0f4cb819951ebbb67c4
Vishal M <manjrekarv48@gmail.com>
2025-01-30 10:22:00 +0530

    test: returns the number itself on single number

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,8 @@ const add = require("./stringCalculator");
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns the number itself on single number", () => {
+  expect(add("1")).toBe(1);
+  expect(add("5")).toBe(5);
+});
