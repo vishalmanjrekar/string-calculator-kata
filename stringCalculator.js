8dca977cd21d02f1927680bf011f91f1a98cc215
Vishal M <manjrekarv48@gmail.com>
2025-01-30 10:30:21 +0530

    test: return sum of two comma-separated numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  return parseInt(numbers);
+  const numArray = numbers.split(",").map(Number);
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -8,3 +8,8 @@ test("returns the number itself on single number", () => {
   expect(add("1")).toBe(1);
   expect(add("5")).toBe(5);
 });
+
+test("returns the sum of two comma-separated numbers", () => {
+  expect(add("1,2")).toBe(3);
+  expect(add("4,5")).toBe(9);
+});
